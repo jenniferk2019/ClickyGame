a1ebf13f93853868f92020227ce8cb68d8d5db80
jenniferk2019 <51275153+jenniferk2019@users.noreply.github.com>
2019-08-27 10:08:33 -0400

    css final test to move img to src

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,14 +1,5 @@
 import React from "react";
 import "./style.css";
-import img from "./img/schoolbus.jpg"
-
-const styles = {
-  backgroundImage: `url(${img})`,
-  backgroundRepeat: 'no-repeat',
-  backgroundPosition: 'center',
-  backgroundSize: 'auto',
-  paddingLeft: '22px'
-}
 
 function NavBar() {
   return (
@@ -16,7 +7,7 @@ function NavBar() {
       <nav class="navbar navbar-light">
         <a class="navbar-brand" id="navbar" href="/">RECESS CLICKY GAME</a>
       </nav>
-      <div className="jumbotron" style={styles}>
+      <div className="jumbotron">
         <h1>Play Clicky Game!</h1>
         <p>
           Click on an image to earn points, but don't click on the same image more than once! 
