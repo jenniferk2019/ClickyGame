4fdf04f31944258dda2bca19503dd9bdaa5ccc96
jenniferk2019 <51275153+jenniferk2019@users.noreply.github.com>
2019-08-27 09:55:46 -0400

    Background image CSS changes

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "./style.css";
 
+const styles = {
+  backgroundImage: 'url("/img/schoolbus.jpg")',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'center',
+  backgroundSize: 'auto',
+  paddingLeft: '22px'
+}
+
 function NavBar() {
   return (
     <div className="container">
       <nav class="navbar navbar-light">
         <a class="navbar-brand" id="navbar" href="/">RECESS CLICKY GAME</a>
       </nav>
-      <div className="jumbotron">
+      <div className="jumbotron" style={styles}>
         <h1>Play Clicky Game!</h1>
         <p>
           Click on an image to earn points, but don't click on the same image more than once! 
